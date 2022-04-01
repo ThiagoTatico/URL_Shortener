d5491ca150c70b45c8308c7fdc069eb0c2613af4
Ariun <thiagotatico007@gmail.com>
2022-04-01 15:52:52 -0300

    Final Refactoring

--- src/db/init.js
+++ src/db/init.js
@@ -3,9 +3,12 @@ const Database = require('./config')
 const initDb = {
   async init() {
     const db = await Database()
-    
 
-
+    await db.exec(`CREATE TABLE urls (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      code TEXT,
+      url TEXT
+    )`)
 
     await db.close()
   }
--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,43 @@
 const express = require('express')
 const routes = express.Router()
 const RoutesUtils = require('./utils/RoutesUtils')
+const Database = require('../src/db/config')
 
+// Enabling the shortened link
+routes.get('/:code', async (req, res, next) => {
+  const code = req.params.code
 
+  const db = await Database()
+  const dbData = await db.get(`SELECT url FROM urls WHERE code = "${code}"`)
+  await db.close()
 
+  if (!dbData) return res.sendStatus(404)
 
-/* GET home page. */
+  res.redirect(dbData.url) // mostrar a url original salva no DB
+})
+
+// GET home page.
 routes.get('/', (req, res, next) => {
   res.render('index', { title: 'Link Shortener' })
 })
 
+// Send URL Form and save in to DB
 routes.post('/new', async (req, res, next) => {
-  const url = req.body.url
   const code = RoutesUtils.generateCode()
+  const url = req.body.url
 
-  const resultado = await Link.create({  //insert into Database nos campos url e code Arrumar
-    url,
-    code
-  })
-  res.render('stats', resultado.dataValues);
+  const db = await Database()
+  const dbData = await db.run(`INSERT INTO urls (
+    code,
+    url
+ ) VALUES (
+     "${code}",
+     "${url}"
+);`)
+
+  await db.close()
+
+  res.render('stats', {code: code, url: url})
 })
 
-module.exports = routes;
+module.exports = routes
