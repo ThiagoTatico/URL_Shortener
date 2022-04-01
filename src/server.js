37dfbd19438f19f96fdce1ffdf9640b48bc85557
Ariun <thiagotatico007@gmail.com>
2022-04-01 10:57:33 -0300

    Initial Commit

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const routes = require('./routes')
+const server = express()
+
+// Template engine
+server.set('view engine', 'ejs')
+
+// Change views folder location
+server.set('views', path.join(__dirname, 'views'))
+
+// Enable static files
+server.use(express.static('public')) // 'Use' is for add functionalities in the server
+
+// Ability req.body
+server.use(express.urlencoded({extended: true}))
+
+// Routes
+server.use(routes)
+
+// Server ON message
+server.listen(3000, () => console.log('Server ON'))
